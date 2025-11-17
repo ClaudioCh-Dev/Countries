a8e687f5a599671a707bd01bf04f155b9d3b3226
Angelo <rideralucar@gmail.com>
2025-11-17 14:27:23 -0500

    actualizacion

--- api/src/db.js
+++ api/src/db.js
@@ -6,9 +6,19 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: isProduction 
+    ? {
+        ssl: {
+          require: true,
+          ca: process.env.CA_CERT,
+        }
+      }
+    : {}
 });
 const basename = path.basename(__filename);
 
